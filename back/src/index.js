a82f609395d0cc79502dff0aeea2bc837b4b0e0e
Indiara Duarte <indiara96d@gmail.com>
2020-08-22 16:34:35 -0300

    Adding validateProjectId middleware to methods POST and DELETE

--- back/src/index.js
+++ back/src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { response } = require('express');
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
@@ -8,14 +8,30 @@ app.use(express.json());
 
 const projects = [];
 
-
-function logRequest(request, response, next){
+function logRequests (request, response, next){
     const { method, url } = request;
 
-    const logLabel = `[${ method.toUpperCase()} ${url} }]`;
+    const logLabel = `[${method.toUpperCase()}] ${url}`;
 
-    console.log(logLabel);
+    console.time(logLabel);
+    
+    next();
+
+    console.timeEnd(logLabel);
 }
+
+function validateProjectId (request, response, next){
+    const { id } = request.params;
+
+    if(!isUuid(id)){
+        return response.status(400).json({ error: 'Invalid project ID'});
+    }
+
+    return next();
+}
+
+app.use(logRequests);
+app.use('/projects/:id', validateProjectId);
 
 app.get('/projects', (request, response) => {
     const { title } = request.query;
@@ -37,7 +53,7 @@ app.post('/projects', (request, response) => {
     return response.json(project);
 });
 
-app.put('/projects/:id', (request, response) => {
+app.put('/projects/:id', validateProjectId, (request, response) => {
     const { id } = request.params;
     const { title, owner } = request.body;
 
@@ -59,7 +75,7 @@ app.put('/projects/:id', (request, response) => {
 });
 
 
-app.delete('/projects/:id', (request, response) => {
+app.delete('/projects/:id', validateProjectId, (request, response) => {
     const { id } = request.params;
 
     const projectIndex = projects.findIndex(project => project.id == id);
