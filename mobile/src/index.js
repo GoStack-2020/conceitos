33b5f6edb37691a07735565cd853f86a77303629
Indiara Duarte <indiara96d@gmail.com>
2020-08-29 19:15:45 -0300

    Connecting app to api

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,31 +1,75 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native';
+import api from './services/api'
 
 // View: div, footer, header, main, aside, section
 // Text: p, span, strong, h1, h2, h3
 
 export default function App() {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('/projects').then(response => {
+            console.log(response.data);
+            setProjects(response.data);
+        });
+    }, []);
+
+    async function handleAddProject() {
+        const response = await api.post('/projects', {
+            title: `Novo projeto ${Date.now()}`,
+            owner: "Indiara Duarte"
+        });
+
+        const project = response.data;
+
+        setProjects([...projects, project]);
+    }
+
     return (
         <>
             <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-            <View style={styles.container}>
-                <Text style={styles.title}>Hello</Text>
-            </View>
+            <SafeAreaView style={styles.container}>
+                <FlatList
+                    data={projects}
+                    keyExtractor={project => project.id}
+                    renderItem={({ item: project }) => (
+                        <Text style={styles.project}>{project.title}</Text>
+                    )}
+                />
+
+                <TouchableOpacity
+                    activeOpacity={0.6}
+                    style={styles.button}
+                    onPress={handleAddProject}
+                >
+                    <Text style={styles.buttonText}>Adicionar projeto</Text>
+                </TouchableOpacity>
+            </SafeAreaView>
         </>
     );
 }
-
 
 const styles = StyleSheet.create({
     container: {
         backgroundColor: '#7159c1',
         flex: 1,
+    },
+
+    project: {
+        fontSize: 30,
+        color: '#FFF',
+    },
+    button: {
+        backgroundColor: '#FFF',
+        margin: 20,
+        height: 50,
+        borderRadius: 4,
         justifyContent: 'center',
         alignItems: 'center',
     },
-
-    title: {
-        fontSize: 32,
-        color: '#FFF',
-    }
+    buttonText: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
 });
