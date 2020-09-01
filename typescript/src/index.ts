709a17fbfb1d32c5009368c37f95c075ad2b3d9c
Indiara Duarte <indiara96d@gmail.com>
2020-08-31 22:38:22 -0300

    Adding const types

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import createUser from './services/CreateUser';
 
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.json( {message: 'Hello'});
+    const user = createUser({
+        name: 'Indiara',
+        email: 'indiara@mail.com',
+        password: '12345',
+        techs: ['Node', 'React'],
+    });
+
+    return response.json({ message: 'Hello' });
 })
 
 app.listen(3333);
